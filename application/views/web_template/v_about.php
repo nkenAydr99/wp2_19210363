e57487a77819a500299ce96fcd0eb98ed892c147
nkenAydr99 <nikenayudr2003@gmail.com>
2022-10-04 13:04:08 +0700

    Membuat Templating Website Sederhana

---
+++ application/views/web_template/v_about.php
@@ -0,0 +1,24 @@
+<section>
+		<h1><?php echo $judul ?></h1>
+	 <h4>Nama</h4>
+	 <ul type="disc">
+		 <li>Nama Depan : Niken Ayu</li>
+		 <li>Nama Belakang : Dara</li>
+	 </ul>
+	 <br>
+	 <h4>Alamat</h4>
+	 <ul type="none">
+		<li> Perumnas 2 Jl. Alpukat 1 No.15 ParungPanjang</li>
+	 </ul>
+
+	 <h4>Tempat Lahir</h4>
+	 <ul type="none">
+		<li>Tangerang</li>
+	 </ul>
+	 
+	 <h4>Olah Raga Favorit</h4>
+	 <ul type="square">
+		<li>Bulutangkis</li>
+		<li>Basket</li>
+	 </ul>
+</section>
